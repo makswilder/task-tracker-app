2023a7f8c26e16957c1a914ee5910772bb8fd0de
makswilder <maks.shherbakov04@gmail.com>
2025-06-15 18:30:46 +0200

    Implemented Entity Relationships

--- src/main/java/com/task/tracker/entities/TaskList.java
+++ src/main/java/com/task/tracker/entities/TaskList.java
@@ -3,6 +3,8 @@ package com.task.tracker.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -19,9 +21,51 @@ public class TaskList {
     @Column(name = "description", nullable = true)
     private String description;
 
+    @OneToMany(
+            mappedBy = "taskList",
+            cascade = { CascadeType.REMOVE, CascadeType.PERSIST}
+            )
+    private List<Task> tasks;
+
     @Column(name = "created", nullable = false)
     private LocalDateTime created;
 
     @Column(name = "updated", nullable = false)
     private LocalDateTime updated;
+
+    public TaskList() {
+    }
+
+    public TaskList(UUID id, String title, String description, List<Task> tasks, LocalDateTime created, LocalDateTime updated) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.tasks = tasks;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        TaskList taskList = (TaskList) o;
+        return Objects.equals(id, taskList.id) && Objects.equals(title, taskList.title) && Objects.equals(description, taskList.description) && Objects.equals(tasks, taskList.tasks) && Objects.equals(created, taskList.created) && Objects.equals(updated, taskList.updated);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, tasks, created, updated);
+    }
+
+    @Override
+    public String toString() {
+        return "TaskList{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", tasks=" + tasks +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
 }
